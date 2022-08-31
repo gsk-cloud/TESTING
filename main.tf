d84e1a5b687a9805a4f6221827634a14e9af09d1
gs_k <gurvinder.kandohla@snapon.com>
2022-08-31 20:38:34 +0100

    chapter 2 - one web server

--- main.tf
+++ main.tf
@@ -11,17 +11,49 @@ terraform {
 
 #provider "aws" {
 #  region = "us-east-1"
+#  region = "us-east-2"
 #  profile = "GSK-1-AWS-Credentials-Profile"
 #}
 
+# TEST - REMOVE
+
 resource "aws_instance" "example" {
   ami                    = "ami-0fb653ca2d3203ac1"
-#  ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  tags =  {
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
     Name = "terraform-example"
   }
 }
 
-# TEST
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
+}
